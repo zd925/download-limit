96b9fd4cab7767e485e04797b8c95184469e5977
daihua <daihua@broada.com>
2020-03-13 17:01:41 +0800

    调整代码

--- src/main/java/com/kongtrio/demo/downloadlimit/inputstream/BandwidthLimiter.java
+++ src/main/java/com/kongtrio/demo/downloadlimit/inputstream/BandwidthLimiter.java
@@ -9,12 +9,13 @@ import org.slf4j.LoggerFactory;
  * <p>
  * 字节流限流器
  * 主要思想：
- * 定义一个chunk以及允许的最大速率 maxRate(单位 KB/s)。
- * 通过maxRate我们可以算出，在maxRate的速率下，通过chunk大小的字节流所需要的时间 timeCostPerChunk
- * 在读取/写入字节时，我们维护已经读取/写入的字节量 bytesWillBeSentOrReceive。
- * 当bytesWillBeSentOrReceive达到chunk的大小时，检查期间过去的时间(nowNanoTime-lastPieceSentOrReceiveTick)
- * 如果过去的时间小于 timeCostPerChunk，说明当前的速率已经超过了 maxRate的速率，这时候就需要休眠一会来限制流量
- * 如果速率没超过或者休眠完后 将bytesWillBeSentOrReceive=bytesWillBeSentOrReceive-chunk
+ * 定义一个chunk(数据块)以及允许的最大速率 maxRate(单位 KB/s)。  
+            (可以计算在阈值的速率时间多少   ,如果正常时间小于计算时间 正常速率比较慢,反之快了,睡一下)
+ * 通过maxRate我们可以算出，在maxRate的速率下，通过chunk大小的字节流所需要的时间 expectByChunkTime
+ * 在读取/写入字节时，我们维护已经读取/写入的字节量 bytesWillBySentOrReceive。
+ * 当bytesWillBySentOrReceive达到chunk的大小时，检查期间过去的时间(nowNanoTime-lastPieceSentOrReceiveTick)
+ * 如果过去的时间小于 expectByChunkTime，说明当前的速率已经超过了 maxRate的速率，这时候就需要休眠一会来限制流量
+ * 如果速率没超过或者休眠完后 将bytesWillBySentOrReceive=bytesWillBySentOrReceive-chunk
  * 之后继续检查
  */
 public class BandwidthLimiter {
@@ -26,13 +27,13 @@ public class BandwidthLimiter {
     private static final Long CHUNK_LENGTH = 1024 * 1024L;
 
     //已经发送/读取的字节数
-    private int bytesWillBeSentOrReceive = 0;
+    private int bytesWillBySentOrReceive = 0;
     //上一次接收到字节流的时间戳——单位纳秒
     private long lastPieceSentOrReceiveTick = System.nanoTime();
     //允许的最大速率，默认为 1024KB/s
     private int maxRate = 1024;
     //在maxRate的速率下，通过chunk大小的字节流要多少时间（纳秒）
-    private long timeCostPerChunk = (1000000000L * CHUNK_LENGTH) / (this.maxRate * KB);
+    private long expectByChunkTime = (1000000000L * CHUNK_LENGTH) / (this.maxRate * KB);
 
     public BandwidthLimiter(int maxRate) {
         this.setMaxRate(maxRate);
@@ -45,9 +46,9 @@ public class BandwidthLimiter {
         }
         this.maxRate = maxRate;
         if (maxRate == 0) {
-            this.timeCostPerChunk = 0;
+            this.expectByChunkTime = 0;
         } else {
-            this.timeCostPerChunk = (1000000000L * CHUNK_LENGTH) / (this.maxRate * KB);
+            this.expectByChunkTime = (1000000000L * CHUNK_LENGTH) / (this.maxRate * KB);
         }
     }
 
@@ -56,12 +57,12 @@ public class BandwidthLimiter {
     }
 
     public synchronized void limitNextBytes(int len) {
-        this.bytesWillBeSentOrReceive += len;
+        this.bytesWillBySentOrReceive += len;
 
-        while (this.bytesWillBeSentOrReceive > CHUNK_LENGTH) {
+        while (this.bytesWillBySentOrReceive > CHUNK_LENGTH) {
             long nowTick = System.nanoTime();
             long passTime = nowTick - this.lastPieceSentOrReceiveTick;
-            long missedTime = this.timeCostPerChunk - passTime;
+            long missedTime = this.expectByChunkTime - passTime;
             if (missedTime > 0) {
                 try {
                     Thread.sleep(missedTime / 1000000, (int) (missedTime % 1000000));
@@ -69,7 +70,7 @@ public class BandwidthLimiter {
                     LOGGER.error(e.getMessage(), e);
                 }
             }
-            this.bytesWillBeSentOrReceive -= CHUNK_LENGTH;
+            this.bytesWillBySentOrReceive -= CHUNK_LENGTH;
             this.lastPieceSentOrReceiveTick = nowTick + (missedTime > 0 ? missedTime : 0);
         }
     }
